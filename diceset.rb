5e8916e32a6e9aec3b351d0579a0c4bdbda64f6c
Prity Ukey <119838901+ukeyprity@users.noreply.github.com>
2022-12-19 12:08:51 +0530

    Add files via upload
    Instead of hardcoding no. and name of players , please accept values at run time and game should be supported for more than 2 players. Also relevant checks to confirm that in no. of players positive integer has been entered.
    [prity]: changes available in greedgame.rb
    
    After a player rolls and the score is calculated, the scoring dice are removed and the player has the option of rolling again using only the non-scoring dice. If all of the thrown dice are scoring, then the player may roll all 5 dice in the next roll. Unable to see implementation for this. ---- [prity]: changes made to diceset.rb
    
    Handling of multiple player names while declaring winner when there is tie.----[prity]: changes available in greedgame.rb
    
    Code can be modularised - we can have separate ruby files for game, player and score calculation classes. ---Done
    
    Need to write each ruby file specific unit tests(rspecs). ---Discussed with Snehi to make it optional

---
+++ diceset.rb
@@ -0,0 +1,44 @@
+#dice and throw logic
+
+class Dice
+	attr_reader :values 
+	def initialize
+		@values = Array.new
+	end
+	def rolling_the_dice(number)
+		@values.clear #clear before start
+		number.times { @values << rand(6) + 1 } #randomly generated values
+		@values
+	end
+	class << self 
+		def score(values) 
+			score = 0
+            no_valuable = values.size
+            values.group_by {|number| number}.each do |key, value|
+               # print "#{key} ,#{value}"
+            quantity = values.length
+                    occurrences_hash = values.group_by(&:itself) #for the occurrences of the values
+                                .transform_values(&:length)
+                    occurrences_hash.each_pair do |number, occurrences|
+                        #print "\n #{number},#{occurrences}"
+                    three_occurances, remaining_occurances = occurrences.divmod(3) # one can use % if needed
+                    if number == 1 # always scoring number 1
+                     score += three_occurances *1000 + remaining_occurances *100
+                        elsif number == 5 # always scoring number 5
+                         score += three_occurances *500 + remaining_occurances*50
+                    else 
+                        score += three_occurances *100*number
+                    end
+                    if(three_occurances == 1) # -----deducting the scoring dices from throw
+                        no_valuable = no_valuable-3
+                    elsif remaining_occurances >1 and (number ==1 || number ==5)
+                        no_valuable = no_valuable-remaining_occurances
+                    end
+                end
+ 			
+     		#no_valuable = 5 if no_valuable == 0 #reset logic
+			return score, no_valuable
+        end
+		end	
+	end
+end
